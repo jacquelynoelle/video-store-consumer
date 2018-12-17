0fe8e8f056d1ca7feba07375975aa7769226535b
Danielle Metzner <danimetzner@gmail.com>
2018-12-17 13:42:59 -0800

    Added react router to the react application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Index = () => <h2>Home</h2>;
+const Search = () => <h2>Search</h2>;
+const Library = () => <h2>Library</h2>;
+const Customers = () => <h2>Customers</h2>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/search/">Search</Link>
+              </li>
+              <li>
+                <Link to="/library/">Library</Link>
+              </li>
+              <li>
+                <Link to="/customers/">Customers</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/search/" component={Search} />
+          <Route path="/library/" component={Library} />
+          <Route path="/customers/" component={Customers} />
+        </div>
+      </Router>
     );
   }
 }

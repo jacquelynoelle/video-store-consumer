245436f804f8260aa0487b42a82c62ca45146db0
Danielle Metzner <danimetzner@gmail.com>
2018-12-20 09:48:58 -0800

    Made the puppet pals link to the home page and removed the home link

--- src/App.js
+++ src/App.js
@@ -101,16 +101,13 @@ class App extends Component {
       <Router>
         <div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <PuppetPals class="navbar-brand logo-image" />
+            <Link to="/" className="nav-link Nav__label"><PuppetPals class="navbar-brand logo-image" /></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
               aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item active">
-                <Link to="/" className="nav-link Nav__label">Home</Link>
-              </li>
               <li className="nav-item">
                 <Link to="/library/" className="nav-link Nav__label">
                   <i className="material-icons">
